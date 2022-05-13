eb356a58910f9d57d3fa5bbf589fcf9c29ac18dd
AhmadJabrayilov <99828519+AhmadJabrayilov@users.noreply.github.com>
2022-05-12 18:00:42 -0800

    Add files via upload

---
+++ Js_image/assets/js/script.js
@@ -0,0 +1,30 @@
+let icon = document.getElementById("icon");
+
+icon.onclick = function () {
+  icon.previousElementSibling.click();
+};
+const fileZone = document.querySelector(".file-zone");
+const gallery = document.querySelector(".gallery .row");
+
+fileZone.addEventListener("dragover", (e) => e.preventDefault());
+
+fileZone.addEventListener("drop", (e) => {
+  e.preventDefault();
+
+  const { dataTransfer } = e;
+
+  let fileReader = new FileReader();
+
+  fileReader.readAsDataURL(dataTransfer.files[0]);
+
+  fileReader.onload = function () {
+    console.log(fileReader.result);
+    gallery.innerHTML += `
+    <div class="col-md-3">
+   
+        <img src="${fileReader.result}" class="img-fluid" alt="" />
+        <button class="btn  dwn-btn">Download  <i id="icon" class="fa-solid fa-file-arrow-down downlad-icon"></i></button>
+    </div>
+    `;
+  };
+});
